a131623029fa2fc56429c050f152c70f743bf50f
MIJI <96563260+KIMMIJIDLQSLEK@users.noreply.github.com>
2022-04-19 11:57:02 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.py
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.py
@@ -1,17 +1,8 @@
-#한번에 계산하기
-
 a,b,v=map(int,input().split())
-if v==a:
-    day=0
+distance=v-a
+if distance%(a-b)!=0:
+    day=distance//(a-b)+1
 else:
-    day=1
+    day=distance//(a-b)
 
-if v-a<=a-b:
-        day+=1
-else:
-    if (v-a)%(a-b)==0: #정확히 나누어 떨어진다면
-        day+=(v-a)//(a-b)
-    else: #나누어 떨어지지않는다면
-        day+=(v-a)//(a-b)+1
-
-print(day)
+print(day+1)
