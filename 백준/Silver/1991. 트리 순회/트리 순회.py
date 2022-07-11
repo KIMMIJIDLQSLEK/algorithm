6714d6bf96f29e43ff36ce03785be0909a77a6fe
MIJI <96563260+KIMMIJIDLQSLEK@users.noreply.github.com>
2022-07-11 17:12:26 +0900

    [Silver I] Title: 트리 순회, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.py
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -1,54 +1,50 @@
-# 입력받은값 트리로 만들기
-# 1. root, left child, right child 입력
-# 2. 입력받은 값 list에 넣기
-#트리순회
-#1. 전위순회
-#2. 중위순회
-#3. 후위순회
+# 1.입력받은값 트리로 만들기
+#  1-1. root, left child, right child 입력
+#  1-1. 입력받은 값 list에 넣기
+# 2. 트리순회
+#  2-1. 전위순회
+#  2-2. 중위순회
+#  2-3. 후위순회
 
 
 tree_list=[0]*1000
 
-preorder_list=[]
+preorder_path=[]
 def preorder(idx):
     if tree_list[idx]=='.':
         return
-    preorder_list.append(tree_list[idx])
+    preorder_path.append(tree_list[idx])
     preorder(idx*2)
     preorder(idx*2+1)
 
-Inorder_list=[]
-def Inorder(idx):
+inorder_path=[]
+def inorder(idx):
     if tree_list[idx]=='.':
         return
-    Inorder(idx*2)
-    Inorder_list.append(tree_list[idx])
-    Inorder(idx*2+1)
+    inorder(idx*2)
+    inorder_path.append(tree_list[idx])
+    inorder(idx*2+1)
 
-postorder_list=[]
+postorder_path=[]
 def postorder(idx):
     if tree_list[idx]=='.':
         return
     postorder(idx*2)
     postorder(idx*2+1)
-    postorder_list.append(tree_list[idx])
+    postorder_path.append(tree_list[idx])
 
 
 n=int(input())
 tree_list[1]='A'
 for i in range(1,n+1):
-    p,lc,rc=map(str,input().split())
-    p_index=tree_list.index(p,1)
-    tree_list[p_index*2]=lc
-    tree_list[p_index*2+1]=rc
-
-# print(tree_list)
+    parent,left_child,right_child=input().split()
+    p_index=tree_list.index(parent,1)
+    tree_list[p_index*2]=left_child
+    tree_list[p_index*2+1]=right_child
 
 preorder(1)
-Inorder(1)
+inorder(1)
 postorder(1)
-print(''.join(preorder_list))
-print(''.join(Inorder_list))
-print(''.join(postorder_list))
-
-
+print(''.join(preorder_path))
+print(''.join(inorder_path))
+print(''.join(postorder_path))
