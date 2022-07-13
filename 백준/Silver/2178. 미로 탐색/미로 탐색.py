6d68610f2c909486e9ccf2df65d4768a876b05c2
MIJI <96563260+KIMMIJIDLQSLEK@users.noreply.github.com>
2022-07-13 21:08:15 +0900

    [Silver I] Title: 미로 탐색, Time: 164 ms, Memory: 116500 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -0,0 +1,32 @@
+from collections import deque
+
+
+n,m=map(int,input().split())
+miro=[]
+
+for i in range(n):
+    miro.append(list(map(int,input())))
+
+#bfs
+def BFS(x,y):
+    dx=[-1,1,0,0] #왼,오,위,아래
+    dy=[0,0,-1,1] #왼,오,위,아래
+    queue=deque()
+    queue.append([y,x])
+    while(queue): #queue가 있을경우
+        y,x=queue.popleft()
+        for i in range(4):
+            nx=x+dx[i]
+            ny=y+dy[i]
+            if nx<0 or ny<0 or nx>=m or ny>=n:
+                continue;
+            if miro[ny][nx]==0:
+                continue;
+            if miro[ny][nx]==1:
+                queue.append([ny,nx])
+                miro[ny][nx]=miro[y][x]+1
+                
+    return miro[n-1][m-1]
+
+#출발위치
+print(BFS(0,0))
