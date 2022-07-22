9469c0bf19c81b6b1b67d12635e9a36e979d8a5f
MIJI <96563260+KIMMIJIDLQSLEK@users.noreply.github.com>
2022-07-22 11:21:26 +0900

    [Silver II] Title: DFS와 BFS, Time: 672 ms, Memory: 32284 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,9 +1,3 @@
-#그래프 만들기
-#노드에 맞는 간선 연결
-#dfs
-#bfs
-
-#dfs
 visited=[]
 def dfs(graph,node):
     visited.append(node)
@@ -11,17 +5,17 @@ def dfs(graph,node):
     for adjacent_node in sorted(graph[node]):
         if adjacent_node not in visited:
             dfs(graph,adjacent_node)
-#bfs
+
 def bfs(graph,node):
     queue=[node]
-    visited=[]
+    visited=[node]
     while queue: #queue가 있을때까지
         current_node=queue.pop(0)
-        visited.append(current_node)
         print(current_node,end=' ')
         for adjacent_node in sorted(graph[current_node]):
-            if adjacent_node not in visited and adjacent_node not in queue:
+            if adjacent_node not in visited:
                 queue.append(adjacent_node)
+                visited.append(adjacent_node)
 
 
 
