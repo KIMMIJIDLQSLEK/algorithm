70aa13a6d16dcc55810ad8f65f7a667438d128e4
MIJI <96563260+KIMMIJIDLQSLEK@users.noreply.github.com>
2022-11-30 11:27:01 +0900

    [level 2] Title: 숫자의 표현, Time: 3.07 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.py
@@ -1,21 +1,14 @@
-#n보다 작은수들의 연속된 수로 표현하기
-#n//2까지의 값만 비교하여 slicing(2중 for문)
-#결과값 +1(자기 자신)
-
 def solution(n):
+    
     cnt=0
-    for back in range(n//2+1,0,-1):
-        # print(back)
-        sum=back
-        for slide in range(back-1,0,-1):
-            sum+=slide
-
+    for i in range(1,n//2+1):
+        sum=0
+        for j in range(i,n):
+            sum+=j
             if sum==n:
                 cnt+=1
                 break;
-            elif sum>n:
+            if sum>n:
                 break;
 
-    
     return cnt+1
-
