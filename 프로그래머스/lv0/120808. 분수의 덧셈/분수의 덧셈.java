837491cabe641bab6988fe306bd1602c6b3056e7
MIJI <96563260+KIMMIJIDLQSLEK@users.noreply.github.com>
2023-01-03 22:38:17 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.13 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[] solution(int denum1, int num1, int denum2, int num2) {
+        int[] answer=new int[2];
+        //분자 구하기
+        int denum=denum1*num2+denum2*num1;
+        //분모 구하기
+        int num=num1*num2;
+        //분자와 분모의 공배수 구하기
+        int min_num=0;
+        if(denum<num){
+            min_num=denum;
+        }
+        else{
+            min_num=num;
+        }
+        for(int i=min_num;i>=2;i--)
+        {
+            if(denum%i==0 && num%i==0)
+            {
+                denum/=i;
+                num/=i;
+                break;
+            }
+
+        }
+        
+                
+        answer[0]=denum;
+        answer[1]=num;
+        
+        return answer;
+    }
+}
