613897e75888e1ab3ba40d9122bb9bb2ef47af70
MIJI <96563260+KIMMIJIDLQSLEK@users.noreply.github.com>
2023-01-27 16:59:50 +0900

    [level 1] Title: 모의고사, Time: 0.98 ms, Memory: 83.8 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.java
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.java
@@ -6,30 +6,30 @@ class Solution {
         int[] person2={2,1,2,3,2,4,2,5};
         int[] person3={3,3,1,1,2,2,4,4,5,5};
         
-        int[] cnt=new int[3];
+        int[] grade=new int[3];
         
         for(int i=0;i<answers.length;i++){
             if(person1[i%5]==answers[i]){
-                cnt[0]++;
+                grade[0]++;
             }
             if(person2[i%8]==answers[i]){
-                cnt[1]++;
+                grade[1]++;
             }
             if(person3[i%10]==answers[i]){
-                cnt[2]++;
+                grade[2]++;
             }
         }
         
         int maxx=Integer.MIN_VALUE;
         for(int i=0;i<3;i++){
-            if(maxx<cnt[i]){
-                maxx=cnt[i];
+            if(maxx<grade[i]){
+                maxx=grade[i];
             }
         }
         
         ArrayList answer=new ArrayList();
         for(int i=0;i<3;i++){
-            if(maxx==cnt[i]){
+            if(maxx==grade[i]){
                 answer.add(i+1);
             }
         }
