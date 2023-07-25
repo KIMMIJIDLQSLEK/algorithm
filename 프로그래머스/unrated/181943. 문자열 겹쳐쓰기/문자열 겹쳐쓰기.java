a25d4d3e015334bc23b53c6225fe301acf15c848
MIJI <96563260+KIMMIJIDLQSLEK@users.noreply.github.com>
2023-07-25 12:52:00 +0900

    [unrated] Title: 문자열 겹쳐쓰기, Time: 1.37 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -1,16 +1,9 @@
 class Solution {
     public String solution(String my_string, String overwrite_string, int s) {
-        String answer = my_string.substring(0,s);
-        
-        int strNum=my_string.length()-s;
-        if(strNum==overwrite_string.length()){
-            return answer+overwrite_string;
-        }else if(strNum>overwrite_string.length()){
-            return answer+overwrite_string+my_string.substring(s+overwrite_string.length(),my_string.length());
-            
+        String answer=my_string.substring(0,s)+overwrite_string;
+        if(my_string.length()>answer.length()){
+            answer+=my_string.substring(s+overwrite_string.length());
         }
-        else{
-            return answer+overwrite_string.substring(strNum);
-        }
+        return answer;
       }
 }
