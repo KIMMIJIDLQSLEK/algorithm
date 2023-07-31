c35c8e3fac326b343c36caaa9986f00b202183c5
MIJI <96563260+KIMMIJIDLQSLEK@users.noreply.github.com>
2023-07-31 22:59:33 +0900

    [unrated] Title: 카운트 업, Time: 1.70 ms, Memory: 71 MB -BaekjoonHub

--- 프로그래머스/unrated/181920. 카운트 업/카운트 업.java
+++ 프로그래머스/unrated/181920. 카운트 업/카운트 업.java
@@ -1,12 +1,7 @@
+import java.util.stream.*;
+
 class Solution {
     public int[] solution(int start, int end) {
-        int[] answer=new int[end-start+1];
-        
-        int value=start;
-        for(int i=0;i<=end-start;i++){
-            answer[i]=value++;
-        }
-        
-        return answer;
+        return IntStream.rangeClosed(start,end).toArray();
     }
 }
