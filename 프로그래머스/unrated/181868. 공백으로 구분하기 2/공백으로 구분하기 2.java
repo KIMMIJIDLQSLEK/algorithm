9c7ea0fea0b6474a7097d14aacd285ce03798887
MIJI <96563260+KIMMIJIDLQSLEK@users.noreply.github.com>
2023-08-22 18:00:04 +0900

    [unrated] Title: 공백으로 구분하기 2, Time: 0.05 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
+++ 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -1,16 +1,6 @@
-import java.util.*;
 class Solution {
     public String[] solution(String my_string) {
-        String[] str=my_string.split(" ");
-        
-        ArrayList<String> list=new ArrayList();
-        for(int i=0;i<str.length;i++){
-            if(!str[i].equals("")){
-                list.add(str[i]);
-            }
-        }
-        
-        String[] answer=list.toArray(new String[list.size()]);
+        String[] answer = {};
         return answer;
     }
 }
