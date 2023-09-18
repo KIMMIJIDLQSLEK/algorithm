7615c798e8b5400087eba798844e54d04c26cddd
MIJI <96563260+KIMMIJIDLQSLEK@users.noreply.github.com>
2023-09-18 22:32:47 +0900

    [unrated] Title: 덧칠하기, Time: 1.32 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
@@ -1,28 +1,15 @@
 class Solution {
     public int solution(int n, int m, int[] section) {
-        int[] paint=new int[n+1];
         int answer=0;
+        int last=0;
         
         for(int i=0;i<section.length;i++){
-            paint[section[i]]=1;
-        }
-        
-        int start=1;
-        while(start<=n){
-            if(paint[start]==0){
-                start++;
-                continue;
-            } 
-            
-            for(int i=0;i<m;i++){
-                if(start+i>n) break;
-                paint[start+i]=0;
+            if(last<section[i]){
+                last=section[i]+m-1;
+                answer++;
             }
-            answer++;
-            
         }
         
         return answer;
-        
     }
 }
